d83028a63c5f68922776a7f3f40857cd0029e268
MauricioBCastro <mauriciobttcastro@gmail.com>
2021-09-27 22:12:45 -0300

    Featured Movie items
    Featured Movie items

--- src/components/FeaturedMovie.js
+++ src/components/FeaturedMovie.js
@@ -3,16 +3,21 @@ import './FeaturedMovie.css';
 
 export default ({item}) => {
     return (
-        <div>
-            <section className="featured" style={{
-                backgroundSize: 'cover',
-                backgroundPosition: 'center',
-                backgroundImage: `url(https://image.tmdb.org/t/p/originals${item.backdrop_path})`
-            }}>
-                <div>
-                    {item.original_name}
+        <section className="featured" style={{
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            backgroundImage: `url(https://image.tmdb.org/t/p/original${item.backdrop_path})`
+        }}>
+            <div className="featured--vertical">
+                <div className="featured--horizontal">
+                    <div className="featured--name">{item.original_name}</div>
+                    <div className="featured--info">
+                        <div className="featured--points">{item.vote_average} Pontos</div>
+                        <div className="featured--year">2099</div>
+                        <div className="featured--seasons">{item.number_of_seasons} Temporadas{item.number_of_seasons !== 1 ? 's' : ''}</div>
+                    </div>
                 </div>
-            </section>
-        </div>
+            </div>
+        </section>
     );
 }
