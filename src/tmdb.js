17f21be4eea736270101f4f8ecda1af596e1ec8c
MauricioBCastro <mauriciobttcastro@gmail.com>
2021-09-27 21:08:32 -0300

    GetMovieID/Featured
    Function for get random movie or tv serie in featured

--- src/tmdb.js
+++ src/tmdb.js
@@ -1,3 +1,5 @@
+import MovieRow from "./components/MovieRow";
+
 const API_KEY = '719456927a9961c0c66d16e6864d503e';
 const API_BASE = 'https://api.themoviedb.org/3';
 
@@ -65,5 +67,24 @@ export default {
                 items: await basicFetch(`/discover/movie?with_genres=99&language=pt-BR&api_key=${API_KEY}`)
             }
         ];
+    },
+    getMovieInfo: async (movieId, type) => {
+        let info = {};
+
+        if(movieId) {
+            switch(type) {
+                case 'movie':
+                    info = await basicFetch(`/movie/${movieId}?language=pt-BR&api_key=${API_KEY}`);
+                break;
+                case 'tv':
+                    info = await basicFetch(`/tv/${movieId}?language=pt-BR&api_key=${API_KEY}`);
+                break;
+                default:
+                    info = null;
+                break;
+            }
+        }
+
+        return info;
     }
 }
